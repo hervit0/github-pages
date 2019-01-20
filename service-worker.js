a1f52ca627a6539beb53552f21f34f4beed5e8e9
Hervito <herve.ahleung@gmail.com>
2019-01-20 17:29:10 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/github-pages/precache-manifest.987a1269bbbb26d00ab8729e46ceb398.js"
+  "/github-pages/precache-manifest.db00f098a0054a86c31af2a17d6a15bc.js"
 );
 
 workbox.clientsClaim();
