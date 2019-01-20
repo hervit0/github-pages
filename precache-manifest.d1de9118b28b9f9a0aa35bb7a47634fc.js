ba5ed22b8c5402fe5de56024f297111d1366008d
Hervito <herve.ahleung@gmail.com>
2019-01-20 15:25:20 +0000

    Updates

---
+++ precache-manifest.d1de9118b28b9f9a0aa35bb7a47634fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4fcd7b72baac77b3176e",
+    "url": "/github-pages/static/css/main.ec40b11e.chunk.css"
+  },
+  {
+    "revision": "4fcd7b72baac77b3176e",
+    "url": "/github-pages/static/js/main.4fcd7b72.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/github-pages/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "a3b2e8550d7ca80447ec",
+    "url": "/github-pages/static/js/runtime~main.a3b2e855.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9d9c1df60aa2fd394d21537085efbe1d",
+    "url": "/github-pages/index.html"
+  }
+];
