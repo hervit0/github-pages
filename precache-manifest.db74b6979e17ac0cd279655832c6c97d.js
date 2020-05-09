e44f31a0696bac46f3b74e8e24c7c0aa8eeb26d6
hervit0 <herve.ahleung@gmail.com>
2020-05-09 18:49:08 +0000

    Automated deployment to GitHub Pages: 5d0719210df67ddeec78dded643ac4bf10093392

---
+++ precache-manifest.db74b6979e17ac0cd279655832c6c97d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9d4ab67e3940f14dfb23ac1cc378ad8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0abcc4fe6b3ec9e4c738",
+    "url": "/static/css/main.6b872b31.chunk.css"
+  },
+  {
+    "revision": "7343c724fec0f67e2d46",
+    "url": "/static/js/2.0353aa05.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.0353aa05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0abcc4fe6b3ec9e4c738",
+    "url": "/static/js/main.a84cf48d.chunk.js"
+  },
+  {
+    "revision": "8eb2bf4073da668d4eb9",
+    "url": "/static/js/runtime-main.e620ecfb.js"
+  },
+  {
+    "revision": "77943a6c8b506737d091f721f6f9169d",
+    "url": "/static/media/begin-random.77943a6c.jpg"
+  },
+  {
+    "revision": "7895e492747c35b6bf409c3163024ab2",
+    "url": "/static/media/begin.7895e492.jpg"
+  },
+  {
+    "revision": "e1b7ca6ad1f2349655324756d5febcb2",
+    "url": "/static/media/book.e1b7ca6a.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
